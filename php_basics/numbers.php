7c930b549bbb6568e4763f9254988465904c27e3
Chris Davis <chris.davis5440@gmail.com>
2016-11-22 16:17:17 -0500

    Added numbers.php working with number variables

---
+++ php_basics/numbers.php
@@ -0,0 +1,16 @@
+<?php
+
+//Working with number variables
+
+$num_one = 1;
+$num_two = 2;
+$num_three = 3;
+$num_four = "4";
+
+var_dump($num_one);
+var_dump($num_two);
+var_dump($num_three);
+var_dump($num_four);
+var_dump($num_one + $num_three);
+
+?>
