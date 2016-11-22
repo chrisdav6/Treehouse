3b231dfcac8b19a1a7d98fe0cd9cbb8100839e14
Chris Davis <chris.davis5440@gmail.com>
2016-11-22 15:48:54 -0500

    Added comments to 01-hello.php

--- php_basics/01-hello.php
+++ php_basics/01-hello.php
@@ -1,5 +1,23 @@
 <?php
 
-echo "Hello";
+/*
+* Doc Lock Comment
+*
+* @author: Chris Davis
+*/
+
+//single line comment
+
+//our first php script
+echo "Hello"; //done
+
+//another script
+echo "Hello Treehouse!"; //done
+
+/*
+multiple
+line
+comment
+*/
 
 ?>
