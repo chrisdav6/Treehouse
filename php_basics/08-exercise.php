6b08dd2e5f60941e14d16119d28fc273d0066b1c
Chris Davis <chris.davis5440@gmail.com>
2016-11-30 15:00:31 -0500

    Planning out exercise schedule app

---
+++ php_basics/08-exercise.php
@@ -0,0 +1,11 @@
+<?php
+
+//PHP Exercise schedule app
+
+//Store each exercise in a string variable
+//Create a variable containing the day of the week
+//Use an if statement to test for the day of the week
+//Display the exercise string for that given day of the week
+
+
+?>
