8b5287f4a145d9fcf256ed2973dc51a0a53bd835
Chris Davis <chris.davis5440@gmail.com>
2016-11-30 13:27:25 -0500

    Updated strings example

--- php_basics/06-strings.php
+++ php_basics/06-strings.php
@@ -2,13 +2,13 @@
 
 //Single Quotes will not expand variable values or work with escape sequences such as \n
 
-$name = "Chris";
+$name = "Ya Dingus!";
 $string_one = 'Hello $name\n';
 echo($string_one);
 
 //Double Quotes will expand variable values and work with escape sequences such as \n
 
-$name2 = "Chris2";
+$name2 = "Symphia Dangus!";
 $string_two = "\nHello $name2\n";
 echo($string_two);
 
