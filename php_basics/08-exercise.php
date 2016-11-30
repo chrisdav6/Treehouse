059887caf1fe774d3d4e7b61dbfd3f21c9847a30
Chris Davis <chris.davis5440@gmail.com>
2016-11-30 15:06:09 -0500

    Updated app with 7 string type variables describing exercises for each day of the week

--- php_basics/08-exercise.php
+++ php_basics/08-exercise.php
@@ -3,6 +3,14 @@
 //PHP Exercise schedule app
 
 //Store each exercise in a string variable
+$exercise1 = "Display strings to the screen";
+$exercise2 = "Working with integers";
+$exercise3 = "Working with floating point numbers";
+$exercise4 = "Working with operators";
+$exercise5 = "Created a simple PHP unit converter";
+$exercise6 = "Working with strings and concatenation";
+$exercise7 = "Learning if/elseif/else logic";
+
 //Create a variable containing the day of the week
 //Use an if statement to test for the day of the week
 //Display the exercise string for that given day of the week
