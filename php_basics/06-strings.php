96121b7f05f2ac04e48fe95fdd98ebce5c8771fa
Chris Davis <chris.davis5440@gmail.com>
2016-11-30 13:02:28 -0500

    Added 06-strings.php

---
+++ php_basics/06-strings.php
@@ -0,0 +1,15 @@
+<?php
+
+//Single Quotes will not expand variable values or work with escape sequences such as \n
+
+$name = "Chris";
+$string_one = 'Hello $name\n';
+echo($string_one);
+
+//Double Quotes will expand variable values and work with escape sequences such as \n
+
+$name2 = "Chris2";
+$string_two = "\nHello $name2\n";
+echo($string_two);
+
+?>
