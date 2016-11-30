568c6adb823687dc38a3482698baecc85114a08f
Chris Davis <chris.davis5440@gmail.com>
2016-11-30 13:42:38 -0500

    Updated 06-strings to include concatenation example

--- php_basics/06-strings.php
+++ php_basics/06-strings.php
@@ -1,15 +1,25 @@
 <?php
 
 //Single Quotes will not expand variable values or work with escape sequences such as \n
-
 $name = "Ya Dingus!";
 $string_one = 'Hello $name\n';
 echo($string_one);
+
+//Single Quotes will expand variable values with concatenation however escape sequences still will not work
+$stringOneConcat = 'Hello '. $name . '\n';
+echo($stringOneConcat);
 
 //Double Quotes will expand variable values and work with escape sequences such as \n
 
 $name2 = "Symphia Dangus!";
 $string_two = "\nHello $name2\n";
 echo($string_two);
+
+//Another way to use concatenation is with the concatenation assignment operator .=
+$name3 = "Barry ";
+$name3 .= "Wilkenson ";
+$name3 .= "Stevenson ";
+$name3 .= "Davidson!\n";
+echo($name3);
 
 ?>
