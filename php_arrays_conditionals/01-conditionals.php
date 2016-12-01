3bf370d0885e4e09fb4c911e0c502ce4a97f63c7
Chris Davis <chris.davis5440@gmail.com>
2016-12-01 12:41:37 -0500

    Using the greater than comparison operator

--- php_arrays_conditionals/01-conditionals.php
+++ php_arrays_conditionals/01-conditionals.php
@@ -12,4 +12,14 @@ if($a == $b) {
   echo("The value of variable <span style='color: red; font-weight: bold;'>\$a</span> $a is NOT equal to the value of variable <span style='color: blue; font-weight: bold;'>\$b</span> $b");
 }
 
+
+//Using the greater than comparison operator to test if one variable is larger in value than the other
+$score = 59;
+
+if($score > 59) {
+  echo("<p style='color: green; font-weight: bold;'>Great Job! Your score is $score, and you completed the level</p>");
+}else {
+  echo("<p style='color: red; font-weight: bold;'>So Sorry! Your score is $score, and you FAILED!</p>");
+}
+
 ?>
