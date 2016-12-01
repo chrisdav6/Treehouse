43831b2182db28bcd095dd0537336f67f06c4e96
Chris Davis <chris.davis5440@gmail.com>
2016-12-01 13:50:07 -0500

    Working with the negation operator

--- php_arrays_conditionals/01-conditionals.php
+++ php_arrays_conditionals/01-conditionals.php
@@ -22,4 +22,27 @@ if($score >= 60) {
   echo("<p style='color: red; font-weight: bold;'>So Sorry! Your score is $score, and you FAILED!</p>");
 }
 
+
+
+//Using the negation operator to test if one variable is not equal in value to the other
+$c = 10;
+$d = 10;
+
+if($c != $d) {
+  echo("The value of variable <span style='color: red; font-weight: bold;'>\$c</span> $c is NOT equal to the value of variable <span style='color: blue; font-weight: bold;'>\$d</span> $d");
+}else {
+  echo("The value of variable <span style='color: red; font-weight: bold;'>\$c</span> $c is equal to the value of variable <span style='color: blue; font-weight: bold;'>\$d</span> $d");
+}
+
+
+//Using the negation operator to test if one variable is not identical in value and type to the other
+$e = 10;
+$f = "10";
+
+if($e !== $f) {
+  echo("<p>The value and type of variable <span style='color: red; font-weight: bold;'>\$e</span> $e is NOT identical to the value and type of variable <span style='color: blue; font-weight: bold;'>\$f</span> $f</p>");
+}else {
+  echo("<p>The value and type of variable <span style='color: red; font-weight: bold;'>\$e</span> $e is identical to the value and type of variable <span style='color: blue; font-weight: bold;'>\$f</span> $f</p>");
+}
+
 ?>
