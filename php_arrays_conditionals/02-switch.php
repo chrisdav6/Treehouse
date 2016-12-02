cadd992a03a5e430f2a8508c270597b3158927ec
Chris Davis <chris.davis5440@gmail.com>
2016-12-02 10:21:00 -0500

    Finished switch statement example

--- php_arrays_conditionals/02-switch.php
+++ php_arrays_conditionals/02-switch.php
@@ -2,6 +2,34 @@
 
 //Working with switch statements
 
+//Day variable for manual manipulation of the switch statement
+$day = "Sunday";
 
+//php date function variable
+$date = date("l");
+
+switch($date) {
+  case "Monday":
+    echo("Today is Monday!");
+    break;
+  case "Tuesday":
+    echo("Today is Tuesday!");
+    break;
+  case "Wednesday":
+    echo("Today is Wednesday!");
+    break;
+  case "Thursday":
+    echo("Today is Thursday!");
+    break;
+  case "Friday":
+    echo("Today is Friday!");
+    break;
+  case "Saturday":
+    echo("Today is Saturday!");
+    break;
+  default:
+    echo("Finally, the day of Rest!");
+    break;
+}
 
 ?>
