c461b8263989b5c51e3875ac9ecfffbb914458c9
Chris Davis <chris.davis5440@gmail.com>
2016-12-02 15:09:17 -0500

    Working with php associative arrays

--- php_arrays_conditionals/05-associativeArrays.php
+++ php_arrays_conditionals/05-associativeArrays.php
@@ -1,6 +1,16 @@
 <?php
 
 //Working with associative arrays
+$carTuners = [
+  "spoon" => "EK9",
+  "realtime" => "TSX",
+  "nismo" => "GTR",
+  "reAmemiya" => "RX7"
+];
 
+$myString = "<p style='color: steelblue; font-size: 20px;'>";
+$myString .= "Spoon Sports has spent many years developing the " . $carTuners["spoon"];
+$myString .= "</p>";
+echo($myString);
 
 ?>
