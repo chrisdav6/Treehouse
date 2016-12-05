d92ad670ec2b7276577af29e50f25cd98cc7439c
Chris Davis <chris.davis5440@gmail.com>
2016-12-05 10:20:11 -0500

    Added 7 todo items as seperate array elements, then added them all to a single multidimensional array

--- php_arrays_conditionals/06-multidimensionalArrays.php
+++ php_arrays_conditionals/06-multidimensionalArrays.php
@@ -1,6 +1,68 @@
 <?php
 
-//Working with multi-dimensional arrays, todo list
+//Working with multi-dimensional arrays,
+//Todo list
+
+$task1 = [
+  "title" => "Wash S2000",
+  "priority" => 1,
+  "due" => "12/8/2016",
+  "complete" => false
+];
+
+$task2 = [
+  "title" => "Store DMV plates for S2000 for winter storage",
+  "priority" => 2,
+  "due" => "12/9/2016",
+  "complete" => false
+];
+
+$task3 = [
+  "title" => "Notify insurance company that S2000 plates have been stored for winter",
+  "priority" => 3,
+  "due" => "12/9/2016",
+  "complete" => false
+];
+
+$task4 = [
+  "title" => "Change Oil in S2000",
+  "priority" => 4,
+  "due" => "12/17/2016",
+  "complete" => false
+];
+
+$task5 = [
+  "title" => "Pump up tires in S2000 to 45psi for winter storage",
+  "priority" => 4,
+  "due" => "12/17/2016",
+  "complete" => false
+];
+
+$task6 = [
+  "title" => "Remove battery from S2000 for winter storage",
+  "priority" => 4,
+  "due" => "12/17/2016",
+  "complete" => false
+];
+
+$task7 = [
+  "title" => "Spring time is here! Change LSD fluid in S2000 and put back on the road! Yay!!!",
+  "priority" => 5,
+  "due" => "4/20/2017",
+  "complete" => false
+];
+
+$list = [
+  $task1,
+  $task2,
+  $task3,
+  $task4,
+  $task5,
+  $task6,
+  $task7
+];
+
+echo($list[0]["title"]);
 
 
 
