a3b5fc406326adba41abff982259c7c6ba919b38
Chris Davis <chris.davis5440@gmail.com>
2016-12-05 13:47:32 -0500

    Sorting indexed arrays

--- php_arrays_conditionals/07-sortingArrays.php
+++ php_arrays_conditionals/07-sortingArrays.php
@@ -2,11 +2,27 @@
 
 //Sorting arrays with asort(), sort() and rsort()
 
-//Sorting an indexed array with asort()
-//asort will reorder the array based on its value and will not re-index array
+//01 Sorting an indexed array with asort()
+//asort() will reorder the array based on its value and will not re-index array
 $myArray = [1,3,0,5];
-echo("My indexed array before asort = " . implode(", ", $myArray));
+echo("<p>My indexed array before asort() = " . implode(", ", $myArray));
 asort($myArray);
-echo(" - My indexed array after asort = " . implode(", ", $myArray));
+echo(" - My indexed array after asort() = " . implode(", ", $myArray) . "</p>");
+
+
+//02 Sorting an indexed array with sort()
+//sort() will reorder the array based on its value and will re-index array
+$myArray = [1,3,0,5];
+echo("<p>My indexed array before sort() = " . implode(", ", $myArray));
+sort($myArray);
+echo(" - My indexed array after sort() = " . implode(", ", $myArray) . "</p>");
+
+
+//03 Sorting an indexed array with rsort()
+//rsort() will reorder the array based on its value in reverse and will re-index array
+$myArray = [1,3,0,5];
+echo("<p>My indexed array before sort() = " . implode(", ", $myArray));
+rsort($myArray);
+echo(" - My indexed array after sort() = " . implode(", ", $myArray) . "</p>");
 
 ?>
