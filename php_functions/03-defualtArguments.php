ef2a22442ab110996dcd38b73ce21874325345e8
Chris Davis <chris.davis5440@gmail.com>
2016-12-07 11:50:26 -0500

    Working with default and optional function arguments

--- php_functions/03-defualtArguments.php
+++ php_functions/03-defualtArguments.php
@@ -1,9 +1,9 @@
 <?php
 
-//Working with defualt function arguments
-//$name = "Chris" will set the argument defualt value to the string "Chris"
+//01-Working with default function arguments
+//$name = "Chris" will set the argument default value to the string "Chris"
 function helloName($name = "Chris") {
-	echo("<p>Hello $name");
+	echo("<p>Hello $name</p>");
 }
 
 //If the argument is left blank when calling the function, the default value is used
@@ -11,5 +11,33 @@ helloName();
 
 //Otherwise, the argument value passed when calling the function is used
 helloName("Scott");
+
+
+//02-Working with multiple default function arguments
+function getInfo($name = "Chris", $title = "Artist") {
+	echo("<p>$name has arrived, the $title");
+}
+
+//Calling the function with default argument values
+getInfo();
+
+//Calling the function with unique argument values
+getInfo("Scott", "Chef");
+
+
+//03-Working with multiple default and optional function arguments
+function getMoreInfo($name = "Chris", $title = null) {
+	if($title) {
+    echo("<p>$name has arrived, the $title");
+  } else {
+    echo("<p>$name has arrived, our friend");
+  }
+}
+
+//Calling the function with $title argument set
+getMoreInfo("Colleen", "Nurse");
+
+//Calling the function with $title argument not set
+getMoreInfo("Bill");
 
 ?>
