6a60d4bd66a908805dc43ba5b9e7691a0e56cde1
Chris Davis <chris.davis5440@gmail.com>
2016-12-07 11:21:16 -0500

    Setting a default function argument value

--- php_functions/03-defualtArguments.php
+++ php_functions/03-defualtArguments.php
@@ -1,6 +1,15 @@
 <?php
 
 //Working with defualt function arguments
+//$name = "Chris" will set the argument defualt value to the string "Chris"
+function helloName($name = "Chris") {
+	echo("<p>Hello $name");
+}
 
+//If the argument is left blank when calling the function, the default value is used
+helloName();
+
+//Otherwise, the argument value passed when calling the function is used
+helloName("Scott");
 
 ?>
