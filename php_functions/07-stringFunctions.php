b14744c22c10fee9c2f9a1834beda8795ca5f5cb
Chris Davis <chris.davis5440@gmail.com>
2016-12-09 14:14:24 -0500

    Working with strlen() to find the length of a string

--- php_functions/07-stringFunctions.php
+++ php_functions/07-stringFunctions.php
@@ -2,5 +2,15 @@
 
 //Working with string functions
 
+//01-strlen() will return the length of a string
+function stringLength($name) {
+	return "<p>The length of the variable \$name with the string value \"" . $name . "\" is " . strlen($name) . " characters</p>";
+}
+
+$chris = stringLength("Chris");
+echo($chris);
+
+$colleen = stringLength("Colleen");
+echo($colleen);
 
 ?>
