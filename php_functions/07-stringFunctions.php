d86bdef293ee2f8e0dfef63a412271304492c0eb
Chris Davis <chris.davis5440@gmail.com>
2016-12-09 14:34:12 -0500

    Working with substr()

--- php_functions/07-stringFunctions.php
+++ php_functions/07-stringFunctions.php
@@ -13,4 +13,25 @@ echo($chris);
 $colleen = stringLength("Colleen");
 echo($colleen);
 
+//strlen() will also count spaces between characters
+$phrase = stringLength("This is an entire sentence!");
+echo($phrase);
+
+
+//02-substr() will return the string starting at the first integer value argument, in this case 5
+function mySubstring($sub){
+	return "<p>The substring of the variable \$sub with the string value \"$sub\" starting at the 5th character is - " . substr($sub, 5) . "</p>";
+}
+
+$subString1 = mySubstring("This is my string");
+echo($subString1);
+
+//if you pass a second integer value argument, that will determing the length of the string
+function mySubstring2($sub){
+	return "<p>The substring of the variable \$sub with the string value \"$sub\" starting at the 5th character and only 5 characters long is - " . substr($sub, 5, 5) . "</p>";
+}
+
+$subString2 = mySubstring2("This is my string");
+echo($subString2);
+
 ?>
