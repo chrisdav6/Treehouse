bbf72135022859d3ec5d271198764faf441ac4b7
Chris Davis <chris.davis5440@gmail.com>
2016-12-12 11:47:02 -0500

    Updated catalog.php pageTitle with a conditional based on the catagory

--- php_basic_website/catalog.php
+++ php_basic_website/catalog.php
@@ -1,10 +1,21 @@
 <?php 
 $pageTitle = "Full Catalog";
+
+if(isset($_GET["cat"])) {
+	if($_GET["cat"] == "books") {
+  		$pageTitle = "Books";
+	}elseif($_GET["cat"] == "movies"){
+  		$pageTitle = "Movies";
+	}elseif($_GET["cat"] == "music"){
+  		$pageTitle = "Music";
+	}
+}
+
 include("includes/header.php"); 
 ?>
 
 <div class="section page">
-  <h1>Full Catalog</h1>
+  <h1><?php echo($pageTitle); ?></h1>
 </div>
 
 <?php include("includes/footer.php"); ?>
