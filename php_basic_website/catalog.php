cead77feb8bf2d435abcd1d8e5b8a76518fbff6f
Chris Davis <chris.davis5440@gmail.com>
2016-12-12 13:03:31 -0500

    Updated method for adding items to array

--- php_basic_website/catalog.php
+++ php_basic_website/catalog.php
@@ -1,9 +1,10 @@
 <?php 
-$catalog = [
-  "Proper Designs",
-  "Building Hondas",
-  "Engine Tuning"
-];
+$catalog = [];
+$catalog[101] = "Design Patterns";
+$catalog[201] = "Forrest Gump";
+$catalog[301] = "Beethoven";
+$catalog[102] = "Clean Code";
+ 
 $pageTitle = "Full Catalog";
 $section = null;
 
