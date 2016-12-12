85c06fa5a91e5041dbfff93eb07d50ac1c837cec
Chris Davis <chris.davis5440@gmail.com>
2016-12-12 12:55:38 -0500

    Built forEach loop structure to list catalog items

--- php_basic_website/catalog.php
+++ php_basic_website/catalog.php
@@ -1,26 +1,40 @@
 <?php 
+$catalog = [
+  "Proper Designs",
+  "Building Hondas",
+  "Engine Tuning"
+];
 $pageTitle = "Full Catalog";
 $section = null;
 
 if(isset($_GET["cat"])) {
-	if($_GET["cat"] == "books") {
+  if($_GET["cat"] == "books") {
       $pageTitle = "Books";
       $section = "books";
-	}elseif($_GET["cat"] == "movies"){
-  		$pageTitle = "Movies";
+  }elseif($_GET["cat"] == "movies"){
+      $pageTitle = "Movies";
       $section = "movies";
-	}elseif($_GET["cat"] == "music"){
-  		$pageTitle = "Music";
+  }elseif($_GET["cat"] == "music"){
+      $pageTitle = "Music";
       $section = "music";
-	}
+  }
 }
 
 include("includes/header.php"); 
 ?>
 
-<div class="section page">
-  <h1><?php echo($pageTitle); ?></h1>
+<div class="section catalog page">
+  <div class="wrapper">
+    <h1><?php echo($pageTitle); ?></h1>
+    <ul>
+      <?php
+      foreach($catalog as $item) {
+        echo("<li>$item</li>");
+      }
+      ?>
+    </ul>
+  </div>
 </div>
 
 <?php include("includes/footer.php"); ?>
-			
+      
