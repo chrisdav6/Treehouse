25a602ee8892428b5b2ad4c969ef910001a5e178
Chris Davis <chris.davis5440@gmail.com>
2016-12-12 11:12:08 -0500

    Moved page footers to the includes folder

--- php_basic_website/index.php
+++ php_basic_website/index.php
@@ -14,22 +14,5 @@
 
 		</div>
 
-	</div><!--End Content-->
-
-	<div class="footer">
-
-		<div class="wrapper">
-
-			<ul>		
-				<li><a href="http://twitter.com/treehouse">Twitter</a></li>
-				<li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
-			</ul>
-
-			<p>&copy;<?php echo(date("Y")); ?> Personal Media Library</p>
-
-		</div>
+<?php include("includes/footer.php"); ?>
 	
-	</div>
-
-</body>
-</html>
