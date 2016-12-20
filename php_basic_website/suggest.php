5c1b9f3b3306cd2a43eff0f89518be95c5d14dc4
Chris Davis <chris.davis5440@gmail.com>
2016-12-20 15:14:57 -0500

    Creating Input Fields

--- php_basic_website/suggest.php
+++ php_basic_website/suggest.php
@@ -12,10 +12,21 @@ include("includes/header.php");
     <p>If you think there is something I&rsquo;m missing, let me know ya dingus! Complete the form to send it to me over the interwebs</p>
     
     <form method="post">
-      
-      <label for="name">Name: </label>
-      <input type="text" name="name" id="name">
-      
+      <table>
+        <tr>
+          <th><label for="name">Name: </label></th>
+          <td><input type="text" name="name" id="name"></td>
+        </tr>
+        <tr>
+          <th><label for="email">Email: </label></th>
+          <td><input type="text" name="email" id="email"></td>
+        </tr>
+        <tr>
+          <th><label for="details">Suggest Details: </label></th>
+          <td><textarea name="details" id="details"></textarea></td>
+        </tr>
+      </table>
+      <input type="submit" value="Send it over ya dingus!">
     </form>
     
   </div>
