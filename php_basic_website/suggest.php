b7020a9bf7c17fea72416a5f03290d9657fb5fa1
Chris Davis <chris.davis5440@gmail.com>
2016-12-20 15:08:02 -0500

    Setting up the initial form

--- php_basic_website/suggest.php
+++ php_basic_website/suggest.php
@@ -5,7 +5,20 @@ include("includes/header.php");
 ?>
 
 <div class="section page">
-  <h1>Suggest a Media Item</h1>
+  <div class="wrapper">
+    
+    <h1>Suggest a Media Item</h1>
+    
+    <p>If you think there is something I&rsquo;m missing, let me know ya dingus! Complete the form to send it to me over the interwebs</p>
+    
+    <form method="post">
+      
+      <label for="name">Name: </label>
+      <input type="text" name="name" id="name">
+      
+    </form>
+    
+  </div>
 </div>
 
 <?php include("includes/footer.php"); ?>
