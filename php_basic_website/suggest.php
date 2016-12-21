f5fc042c0dd1738b241cdacd73b6e7270a56161e
Chris Davis <chris.davis5440@gmail.com>
2016-12-21 09:31:07 -0500

    Checking the request method

--- php_basic_website/suggest.php
+++ php_basic_website/suggest.php
@@ -1,17 +1,43 @@
 <?php 
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $details = $_POST["details"];
+  
+  $message = "Thank you $name for your interest.<br>";
+  $message .= "I have your email address as $email<br>";
+  $message .= "I have also received your message:  $details<br>";
+  $message .= "I will get back to you as soon as i can";
+  $message .= "<p>Best Regards,<br>Chris</p>";
+  
+  echo($message);
+  
+  //Send Email
+  
+  //Redirect to thanks.php
+  header("location:suggest.php?status=thanks");
+}
+
 $pageTitle = "Suggest a Media Item";
 $section = "suggest";
-include("includes/header.php"); 
+include("includes/header.php");
+
 ?>
 
 <div class="section page">
   <div class="wrapper">
     
     <h1>Suggest a Media Item</h1>
-    
+    <?php
+    if(isset($_GET["status"]) && $_GET["status"] == "thanks") {
+      echo("<p>Thanks for the email! I will check out your suggestion</p>");
+    }else { ?>
+      
     <p>If you think there is something I&rsquo;m missing, let me know ya dingus! Complete the form to send it to me over the interwebs</p>
+      
     
-    <form method="post" action="process.php">
+    <form method="post" action="suggest.php">
       <table>
         <tr>
           <th><label for="name">Name: </label></th>
@@ -28,9 +54,9 @@ include("includes/header.php");
       </table>
       <input type="submit" value="Send it over ya dingus!">
     </form>
-    
+    <?php } ?>
   </div>
 </div>
 
 <?php include("includes/footer.php"); ?>
-			
+      
