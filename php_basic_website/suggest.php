42cdc624bb82829bb110664e28cae5f5829129d4
Chris Davis <chris.davis5440@gmail.com>
2016-12-21 10:58:18 -0500

    Using a third party mailer library

--- php_basic_website/suggest.php
+++ php_basic_website/suggest.php
@@ -15,16 +15,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     exit;
   }
   
+  require("includes/phpmailer/class.phpmailer.php");
+  
+  //Send Email
   $message = "Thank you $name for your interest.<br>";
   $message .= "I have your email address as $email<br>";
   $message .= "I have also received your message:  $details<br>";
   $message .= "I will get back to you as soon as i can";
   $message .= "<p>Best Regards,<br>Chris</p>";
-  
   echo($message);
-  
-  //Send Email
-  
+
   //Redirect to thanks.php
   header("location:suggest.php?status=thanks");
 }
