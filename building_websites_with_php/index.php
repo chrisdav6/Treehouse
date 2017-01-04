33c2b9136e613662690b91cab9f29ab34c759cbc
Chris Davis <chris.davis5440@gmail.com>
2017-01-04 13:51:21 -0500

    Autoloading with PHP

---
+++ building_websites_with_php/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require("vendor/autoload.php");
+date_default_timezone_set("America/New_York");
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.txt', Monolog\Logger::WARNING));
+$log->addWarning('Foo');
+
+echo("Hello");
+
+?>
