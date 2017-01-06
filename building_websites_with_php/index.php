d47b8f318ad107fbd53f683752c3f24eeaa0b41b
Chris Davis <chris.davis5440@gmail.com>
2017-01-06 09:46:17 -0500

    Named routes

--- building_websites_with_php/index.php
+++ building_websites_with_php/index.php
@@ -25,11 +25,11 @@ $view->parserExtensions = array(
 
 $app->get('/', function() use($app){
   $app->render('about.twig');
-});
+})->name('home');
 
 $app->get('/contact', function() use($app){
   $app->render('contact.twig');
-});
+})->name('contact');
 
 $app->run();
           
