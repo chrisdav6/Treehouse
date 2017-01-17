8bdba6b59d422a6eb0194b471f0177d1536b0d6d
Chris Davis <chris.davis5440@gmail.com>
2017-01-17 16:04:09 -0500

    Removing names

--- dom_scripting_by_example/app.js
+++ dom_scripting_by_example/app.js
@@ -2,18 +2,9 @@ const form = document.getElementById("registrar");
 const input = document.querySelector("input");
 const ul = document.getElementById("invitedList");
 
-//Create an submit event handler on the form element
+//Function to create li
 
-form.addEventListener("submit", (e) => {
-  //prevent default form behavior from reloading the page on submit
-  e.preventDefault();
-  
-  //Collect input value into a variable
-  const text = input.value;
-  
-  //Clear out input box after value is saved into variable above
-  input.value = '';
-  
+function createLI(text) {
   //Create a new li element
   const li = document.createElement("li");
   
@@ -36,9 +27,37 @@ form.addEventListener("submit", (e) => {
   //Append the label and checkbox to the li
   li.appendChild(label);
   
+  //Create a button element with the text content remove
+  const button = document.createElement("button");
+  button.textContent = "Remove";
+  
+  //Append the button to the li
+  li.appendChild(button);
+  
+  return li;
+}
+
+
+//Create an submit event handler on the form element
+
+form.addEventListener("submit", (e) => {
+  //prevent default form behavior from reloading the page on submit
+  e.preventDefault();
+  
+  //Collect input value into a variable
+  const text = input.value;
+  
+  //Clear out input box after value is saved into variable above
+  input.value = '';
+  
+  //Create the li from the createLI function
+  const li = createLI(text);
+  
   //Append the li to the ul
   ul.appendChild(li);
 });
+
+  
   
 //Create a on change event handler on the checkbox within the ul element
   
@@ -58,7 +77,19 @@ ul.addEventListener("change", (e) => {
   }
 });
   
+
   
+//Create a on click event handler on the button within the ul element
+  
+ul.addEventListener("click", (e) => {
+  //Check to see if the element with a type of button with the text content "Remove" was clicked
+  if(e.target.tagName === "BUTTON" && e.target.textContent === "Remove") {
+    const li = e.target.parentNode;
+    const ul = li.parentNode;
+    //remove li from page
+    ul.removeChild(li);
+  }
+}); 
   
   
   
